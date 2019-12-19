8832d94be3a4bd4f7a655486dbdec8be2b3bc20c
v1rtl <pilll.PL22@gmail.com>
2019-12-19 20:27:40 +0300

    add better error message and help command

--- cli.js
+++ cli.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 const path = require('path')
-const { init, klap, read, log, error } = require('./dist')
+const { init, klap, read, log, error, info } = require('./dist')
 const { name, version } = require('./package.json')
 const command = process.argv[2]
 
@@ -16,6 +16,16 @@ const command = process.argv[2]
       log(`${name}@${version} - Working on ${command}...`)
       const pkg = JSON.parse(await read(path.join(process.cwd(), 'package.json')))
       await klap(command, pkg)
+      break
+    case 'help':
+      info(`
+      Usage:
+    
+      klap init - create a new Klap application
+      klap build - build in production mode
+      klap watch - watches files for changes
+      klap start - start watching and building in development mode
+      `)
       break
     default:
       error('No Such Command !!')
