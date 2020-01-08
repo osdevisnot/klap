f7fed2937b9a1d13aa1631766329505e36d8ea66
Abhishek Shende <osdevisnot@gmail.com>
2020-01-08 00:46:48 -0800

    feat: add support for terser nameCache
    terser nameCache helps ensure a same minified name is used for cjs, esm and umd builds

--- src/packages/terser.js
+++ src/packages/terser.js
@@ -5,10 +5,28 @@
  * - `jest-worker` had issues with bundling approach we want to follow for `klap`.
  */
 
+import { readFileSync, writeFileSync } from 'fs';
+import { join } from 'path';
+import merge from 'deepmerge';
 import { codeFrameColumns } from '@babel/code-frame';
 import { createFilter } from 'rollup-pluginutils';
 import { minify } from 'terser';
+
 import { error } from '../logger';
+
+const cacheFileName = join(process.cwd(), 'node_modules', '.klap.cache.json');
+
+const readNameCache = () => {
+  try {
+    return JSON.parse(readFileSync(cacheFileName, 'utf-8'));
+  } catch (e) {}
+  return {};
+};
+const writeNameCache = nameCache => {
+  try {
+    writeFileSync(cacheFileName, JSON.stringify(nameCache, null, '  ') + '\n');
+  } catch (e) {}
+};
 
 const transform = (code, options) => {
   const result = minify(code, options);
@@ -18,10 +36,25 @@ const transform = (code, options) => {
   return result;
 };
 
+const nameCache = readNameCache();
+
 export const terser = (options = {}) => {
   const filter = createFilter(options.include, options.exclude, {
     resolve: false,
   });
+
+  delete options.include;
+  delete options.exclude;
+
+  const finalOptions = merge(
+    {
+      toplevel: true,
+      mangle: { properties: { regex: '^_|^\\$' } },
+      compress: { passes: 10, pure_getters: true },
+      nameCache,
+    },
+    options
+  );
 
   return {
     name: 'terser',
@@ -31,16 +64,14 @@ export const terser = (options = {}) => {
 
       let result;
       try {
-        result = transform(code, {
-          sourceMap: options.sourcemap,
-          toplevel: true,
-          mangle: { properties: { regex: '^_|^\\$' } },
-          compress: { passes: 10, pure_getters: true },
-        });
+        result = transform(code, finalOptions);
       } catch (err) {
         const { message, line, col: column } = err;
         error(codeFrameColumns(code, { start: { line, column } }, { message }));
         throw err;
+      }
+      if (finalOptions.nameCache) {
+        writeNameCache(finalOptions.nameCache);
       }
       return {
         code: result.code,
