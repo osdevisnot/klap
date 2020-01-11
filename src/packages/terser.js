deac32ceb09730c2d658820d19a0785125d73071
Abhishek Shende <osdevisnot@gmail.com>
2020-01-11 10:36:44 -0800

    Revert "feat: add support for terser nameCache"
    This reverts commit f7fed2937b9a1d13aa1631766329505e36d8ea66.

--- src/packages/terser.js
+++ src/packages/terser.js
@@ -5,28 +5,10 @@
  * - `jest-worker` had issues with bundling approach we want to follow for `klap`.
  */
 
-import { readFileSync, writeFileSync } from 'fs';
-import { join } from 'path';
-import merge from 'deepmerge';
 import { codeFrameColumns } from '@babel/code-frame';
 import { createFilter } from 'rollup-pluginutils';
 import { minify } from 'terser';
-
 import { error } from '../logger';
-
-const cacheFileName = join(process.cwd(), 'node_modules', '.klap.cache.json');
-
-const readNameCache = () => {
-  try {
-    return JSON.parse(readFileSync(cacheFileName, 'utf-8'));
-  } catch (e) {}
-  return {};
-};
-const writeNameCache = nameCache => {
-  try {
-    writeFileSync(cacheFileName, JSON.stringify(nameCache, null, '  ') + '\n');
-  } catch (e) {}
-};
 
 const transform = (code, options) => {
   const result = minify(code, options);
@@ -36,25 +18,10 @@ const transform = (code, options) => {
   return result;
 };
 
-const nameCache = readNameCache();
-
 export const terser = (options = {}) => {
   const filter = createFilter(options.include, options.exclude, {
     resolve: false,
   });
-
-  delete options.include;
-  delete options.exclude;
-
-  const finalOptions = merge(
-    {
-      toplevel: true,
-      mangle: { properties: { regex: '^_|^\\$' } },
-      compress: { passes: 10, pure_getters: true },
-      nameCache,
-    },
-    options
-  );
 
   return {
     name: 'terser',
@@ -64,14 +31,16 @@ export const terser = (options = {}) => {
 
       let result;
       try {
-        result = transform(code, finalOptions);
+        result = transform(code, {
+          sourceMap: options.sourcemap,
+          toplevel: true,
+          mangle: { properties: { regex: '^_|^\\$' } },
+          compress: { passes: 10, pure_getters: true },
+        });
       } catch (err) {
         const { message, line, col: column } = err;
         error(codeFrameColumns(code, { start: { line, column } }, { message }));
         throw err;
-      }
-      if (finalOptions.nameCache) {
-        writeNameCache(finalOptions.nameCache);
       }
       return {
         code: result.code,
