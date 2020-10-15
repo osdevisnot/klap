7eac909c4334c2ea15d9fa86210dc961f780a8c5
osdevisnot <osdevisnot@gmail.com>
2020-10-15 09:04:20 -0700

    chore: sync build output after updates

--- examples/jsx-runtime/dist/index.esm.js
+++ examples/jsx-runtime/dist/index.esm.js
@@ -1,4 +1,4 @@
-import { jsx as r } from 'preact/jsx-runtime'
+import { jsx as r } from 'preact/jsx-runtime.js'
 var e = (r, e) => r + e,
 	o = (e) => r('div', { children: 'Hello World' })
 export { o as MyComponent, e as sum }
