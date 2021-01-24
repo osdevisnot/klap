6d6965526a4213593daa0c43162708e23828f8ad
osdevisnot <osdevisnot@gmail.com>
2021-01-24 10:16:36 -0800

    fix: upgrade dev dependencies

--- examples/dynamic-imports/dist/index.esm.js
+++ examples/dynamic-imports/dist/index.esm.js
@@ -1,8 +1,7 @@
-var r = (r, o) => r + o,
-	o =
-		(Promise.resolve().then(function () {
-			return o
-		}),
-		{ __proto__: null, sub: (r, o) => r - o })
+var r = (r, o) => r + o
+Promise.resolve().then(function () {
+	return o
+})
+var o = { __proto__: null, sub: (r, o) => r - o }
 export { r as sum }
 //# sourceMappingURL=index.esm.js.map
