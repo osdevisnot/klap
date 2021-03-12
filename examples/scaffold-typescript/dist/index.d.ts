4c01cea6229aae20936dbacb8702d4518f08db0b
osdevisnot <osdevisnot@gmail.com>
2021-03-12 11:12:28 -0800

    refactor: refactor examples

---
+++ examples/scaffold-typescript/dist/index.d.ts
@@ -0,0 +1,3 @@
+declare const sum: (a: number, b: number) => number;
+
+export { sum };
--- test.sh
+++ test.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
-
+# set -x
 git clean -fdX
 npm link
 cd examples
 for dir in $(ls)
 do
-	cd ${dir} && klap build && rm -rf dist/*.map && cd ..
+	cd ${dir} && yarn install --prod --silent && klap build && rm -rf dist/*.map && cd ..
 done
 yarn prettier --write "examples/**/dist/*.js"
-
