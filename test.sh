75f7be8ed5a516e507f987925e26a4764e1c33c9
osdevisnot <osdevisnot@gmail.com>
2021-08-13 09:29:06 -0700

    chore: fix travis builds to fail on diff

--- test.sh
+++ test.sh
@@ -5,6 +5,6 @@ npm link
 cd examples
 for dir in $(ls | grep -v const-enums) 
 do
-	cd ${dir} && yarn install --prod --silent && klap build && rm -rf dist/*.map && cd ..
+	cd "${dir}" && yarn install --prod --silent && klap build && rm -rf dist/*.map && cd ..
 done
 yarn prettier --write "examples/**/dist/*.js"
